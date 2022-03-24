ad9f38ed0f04315088c57a4f23bbf37b32e0ef7f
Long Nguyen <anonyhost@gmail.com>
2022-03-24 11:48:30 +0700

    update basic class

--- bm25_ranking/bm25_ranker.py
+++ bm25_ranking/bm25_ranker.py
@@ -9,23 +9,29 @@ from utils.utilities import get_raw_from_preproc
 
 
 class Bm25Ranker:
-    def __init__(self, article_pool: ArticlePool, ques_pool: QuestionPool):
+    def __init__(self, article_pool: ArticlePool, ques_pool: QuestionPool, bm25okapi_pkl: str = None):
         self.article_pool = article_pool
         self.ques_pool = ques_pool
-        corpus = [get_raw_from_preproc(preproc_text) for preproc_text in self.article_pool.proc_text_pool]
-        self.bm25okapi = BM25Okapi(corpus)
+        if bm25okapi_pkl is None:
+            corpus = [get_raw_from_preproc(preproc_text) for preproc_text in self.article_pool.proc_text_pool]
+            self.bm25okapi = BM25Okapi(corpus)
+        else:
+            self.bm25okapi: BM25Okapi = pickle.load(open(bm25okapi_pkl, 'rb'))
         self.__raw_ques = [get_raw_from_preproc(preproc_text) for preproc_text in self.ques_pool.proc_ques_pool]
+
+    def save_bm25okapi(self):
+        pickle.dump(self.bm25okapi, open('pkl_file/bm25okapi.pkl', 'wb'))
 
     def get_topn(self, ques_id: int, top_n: int):
         lis_score = self.bm25okapi.get_scores(self.__raw_ques[ques_id])
         return np.argsort(lis_score)[-top_n:]
 
 
-if __name__ == '__main__':
+def test_pkl_loader():
     ap: ArticlePool = pickle.load(open('pkl_file/article_pool.pkl', 'rb'))
     qp: QuestionPool = pickle.load(open('pkl_file/question_pool.pkl', 'rb'))
-    br: Bm25Ranker = pickle.load(open('pkl_file/bm25_ranker.pkl', 'rb'))
-    # br = Bm25Ranker(ap, qp)
+    # br: Bm25Ranker = pickle.load(open('pkl_file/bm25_ranker.pkl', 'rb'))
+    br = Bm25Ranker(ap, qp, bm25okapi_pkl='pkl_file/bm25okapi.pkl')
     # pickle.dump(br, open('pkl_file/bm25_ranker.pkl', 'wb'))
     test_id = 100
     topn = 1000
@@ -34,3 +40,15 @@ if __name__ == '__main__':
     print(topn_article)
     rela = qp.lis_ques[test_id].relevance_articles[0]
     print(ap.get_position(rela) in topn_article)
+
+
+def build_bm25_and_save():
+    ap: ArticlePool = pickle.load(open('pkl_file/article_pool.pkl', 'rb'))
+    qp: QuestionPool = pickle.load(open('pkl_file/question_pool.pkl', 'rb'))
+    br = Bm25Ranker(ap, qp)
+    br.save_bm25okapi()
+
+
+if __name__ == '__main__':
+    # build_bm25_and_save()
+    test_pkl_loader()
--- data_processor/entities/question.py
+++ data_processor/entities/question.py
@@ -5,5 +5,8 @@ class Question:
     def __init__(self, question_json: dict):
         self.question_id = question_json.get('question_id')
         self.question = question_json.get('question')
-        self.relevance_articles = [ArticleIdentity(article_identity_json)
-                                   for article_identity_json in question_json.get('relevant_articles')]
+        if question_json.get('relevant_articles') is not None:
+            self.relevance_articles = [ArticleIdentity(article_identity_json)
+                                       for article_identity_json in question_json.get('relevant_articles')]
+        else:
+            self.relevance_articles = []
