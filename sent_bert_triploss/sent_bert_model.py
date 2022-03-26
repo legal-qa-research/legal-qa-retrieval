75f43e63ef978e6d29aaa7aefb4b5ce924cc2fa5
Long Nguyen <anonyhost@gmail.com>
2022-03-26 15:24:13 +0700

    determine training layers

--- sent_bert_triploss/sent_bert_model.py
+++ sent_bert_triploss/sent_bert_model.py
@@ -2,9 +2,19 @@ from sentence_transformers import SentenceTransformer, models
 
 from sent_bert_triploss.args_management import args
 
+train_layers = [
+    'auto_model.pooler.dense.bias',
+    'auto_model.pooler.dense.weight'
+]
+
 
 def get_sent_bert_model() -> SentenceTransformer:
     bert_embedding_model = models.Transformer(args.model_name, max_seq_length=args.max_seq_len)
+    for name, params in bert_embedding_model.named_parameters():
+        if name in train_layers:
+            params.requires_grad = True
+        else:
+            params.requires_grad = False
 
     pooling_model = models.Pooling(word_embedding_dimension=bert_embedding_model.get_word_embedding_dimension(),
                                    pooling_mode_cls_token=True)
