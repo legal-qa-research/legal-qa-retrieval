cf2504aafdb0691c2dc4e96609eeb6bf9b2fad13
Long Nguyen <anonyhost@gmail.com>
2022-03-31 12:19:28 +0700

    update load model process

--- sent_bert_triploss/sent_bert_model.py
+++ sent_bert_triploss/sent_bert_model.py
@@ -7,11 +7,11 @@ def get_sent_bert_model(load_chk_point_path) -> SentenceTransformer:
     if load_chk_point_path is not None:
         print(f'Load model from {load_chk_point_path}')
         return SentenceTransformer(model_name_or_path=load_chk_point_path)
+    else:
+        print('Init model from BERT checkpoint')
+        bert_embedding_model = models.Transformer(args.model_name, max_seq_length=args.max_seq_len)
 
-    print('Init model from BERT checkpoint')
-    bert_embedding_model = models.Transformer(args.model_name, max_seq_length=args.max_seq_len)
+        pooling_model = models.Pooling(word_embedding_dimension=bert_embedding_model.get_word_embedding_dimension(),
+                                       pooling_mode_cls_token=True)
 
-    pooling_model = models.Pooling(word_embedding_dimension=bert_embedding_model.get_word_embedding_dimension(),
-                                   pooling_mode_cls_token=True)
-
-    return SentenceTransformer(modules=[bert_embedding_model, pooling_model])
+        return SentenceTransformer(modules=[bert_embedding_model, pooling_model])
