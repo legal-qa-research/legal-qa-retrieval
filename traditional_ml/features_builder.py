63cd6323be01550b55b41be07341ddcb853ac2e1
Long Nguyen <anonyhost@gmail.com>
2022-04-28 04:34:22 +0700

    cal kse feature

--- traditional_ml/features_builder.py
+++ traditional_ml/features_builder.py
@@ -1,18 +1,20 @@
-import json
 import pickle
 from typing import List
 
 import fasttext
 import numpy as np
 from fasttext.FastText import _FastText
+from sklearn.feature_extraction.text import TfidfVectorizer
 from tqdm import tqdm
 
 from traditional_ml.args_management import args
 from traditional_ml.constant import train_examples_path, test_examples_path
 from traditional_ml.data import Data
 from traditional_ml.raw_input_example import RawInputExample
-from utils.constant import pkl_question_pool, pkl_article_pool, pkl_cached_rel, pkl_split_ids
-from utils.utilities import build_vec
+from utils.constant import pkl_question_pool, pkl_article_pool, pkl_cached_rel, pkl_split_ids, pkl_tfidf
+from utils.utilities import calculate_jaccard_sim
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy.sparse import csr_matrix
 
 
 class FeaturesBuilder:
@@ -22,14 +24,45 @@ class FeaturesBuilder:
         self.train_phase_data = Data(pkl_question_pool_path=pkl_question_pool, pkl_article_pool_path=pkl_article_pool,
                                      pkl_cached_rel_path=pkl_cached_rel, pkl_cached_split_ids=pkl_split_ids,
                                      args=self.args)
+        self.tfidf_vtr: TfidfVectorizer = pickle.load(open(pkl_tfidf, 'rb'))
+
+    @staticmethod
+    def cal_avg_word_embed_vec(ft: _FastText, tfidf_vtr: TfidfVectorizer, tfidf_matrix: csr_matrix):
+        v = np.stack(
+            [ft.get_word_vector(tfidf_vtr.get_feature_names_out()[wid]) * tfidf_matrix[0, wid]
+             for wid in tfidf_matrix.nonzero()[1]])
+        return v.mean(axis=0)
+
+    @staticmethod
+    def cal_feature_from_inp(input_exp: RawInputExample, ft: _FastText, tfidf_vtr: TfidfVectorizer,
+                             tfidf_matrix: csr_matrix):
+        ques_art_tfidf = tfidf_vtr.transform([' '.join(input_exp.ques), ' '.join(input_exp.articles)])
+        cosine = cosine_similarity(ques_art_tfidf)
+        ques_vec = FeaturesBuilder.cal_avg_word_embed_vec(ft=ft, tfidf_matrix=ques_art_tfidf[0], tfidf_vtr=tfidf_vtr)
+        art_vec = FeaturesBuilder.cal_avg_word_embed_vec(ft=ft, tfidf_matrix=ques_art_tfidf[1], tfidf_vtr=tfidf_vtr)
+
+        jaccard_score = calculate_jaccard_sim(u=input_exp.ques, v=input_exp.articles)
+        label = input_exp.label
+        return np.concatenate((ques_vec, art_vec, [cosine[0, 1], jaccard_score, label]), axis=0)
+
+    def build_vec(self, ft: _FastText, tfidf_matrix: csr_matrix) -> np.ndarray:
+        v = np.stack(
+            [ft.get_word_vector(self.tfidf_vtr.get_feature_names_out()[wid]) * tfidf_matrix[0, wid]
+             for wid in tfidf_matrix.nonzero()[1]])
+        # v = np.stack([ft.get_word_vector(tok) for tok in lis_tok])
+        return v.mean(axis=0)
 
     def build_feature(self, lis_examples: List[RawInputExample]) -> np.ndarray:
         data = []
         for input_exp in tqdm(lis_examples, desc='Build Features'):
-            ques_vec = build_vec(ft=self.fasttext_model, lis_tok=input_exp.ques)
-            art_vec = build_vec(ft=self.fasttext_model, lis_tok=input_exp.articles)
+            ques_art_tfidf = self.tfidf_vtr.transform([' '.join(input_exp.ques), ' '.join(input_exp.articles)])
+            cosine = cosine_similarity(ques_art_tfidf)
+            ques_vec = self.build_vec(ft=self.fasttext_model, tfidf_matrix=ques_art_tfidf[0])
+            art_vec = self.build_vec(ft=self.fasttext_model, tfidf_matrix=ques_art_tfidf[1])
+
+            jaccard_score = calculate_jaccard_sim(u=input_exp.ques, v=input_exp.articles)
             label = input_exp.label
-            data.append(np.concatenate((ques_vec, art_vec, [label]), axis=0))
+            data.append(np.concatenate((ques_vec, art_vec, [cosine[0, 1], jaccard_score, label]), axis=0))
         return np.stack(data)
 
     def start_build(self, fast_text_type: str = ''):
@@ -43,8 +76,10 @@ class FeaturesBuilder:
 
 
 def test_built_feature():
-    a = np.load('traditional_ml/feature_data/train_data_general.npy')
-    b = np.load('traditional_ml/feature_data/test_data_general.npy')
+    a = np.load('traditional_ml/feature_data/train_data_vnlaw_kse_jac_tfidf.npy')
+    b = np.load('traditional_ml/feature_data/test_data_vnlaw_kse_jac_tfidf.npy')
+    print(a.shape)
+    print(b.shape)
     pass
 
 
@@ -56,6 +91,6 @@ if __name__ == '__main__':
 
     fb = FeaturesBuilder(args=args, fasttext_model_path='traditional_ml/pretrained_fasttext/vnlaw_ft.bin')
     # fb = FeaturesBuilder(args=args, fasttext_model_path='traditional_ml/pretrained_fasttext/wiki.vi.bin')
-    fb.start_build(fast_text_type='general')
+    fb.start_build(fast_text_type='vnlaw_kse_jac_tfidf')
 
     test_built_feature()
