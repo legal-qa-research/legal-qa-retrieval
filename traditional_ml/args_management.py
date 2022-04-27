dade65fa4ea0fc2ae5f544555fc0d4a540f04776
Long Nguyen <anonyhost@gmail.com>
2022-04-28 04:56:26 +0700

    not require args

--- traditional_ml/args_management.py
+++ traditional_ml/args_management.py
@@ -5,6 +5,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument(
     "--is_dev_phase",
     default=0,
+    required=False,
     type=int,
     help="Is in development phase or not",
 )
@@ -12,6 +13,7 @@ parser.add_argument(
 parser.add_argument(
     "--split_ids",
     default=None,
+    required=False,
     type=str,
     help="Split ids cached file",
 )
@@ -19,6 +21,7 @@ parser.add_argument(
 parser.add_argument(
     "--infer_top_k",
     default=2,
+    required=False,
     type=int,
     help="Top-k when inference",
 )
@@ -26,6 +29,7 @@ parser.add_argument(
 parser.add_argument(
     "--infer_threshold",
     default=0,
+    required=False,
     type=float,
     help="Threshold when inference",
 )
