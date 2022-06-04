856f48cd9b0f3b3c82706870534c4d68eef39562
Long Nguyen <anonyhost@gmail.com>
2022-06-04 22:24:35 +0700

    add alqac 2022 config

--- utils/constant.py
+++ utils/constant.py
@@ -2,11 +2,11 @@ import os
 
 pkl_root = 'pkl_file'
 
-pkl_article_pool = os.path.join(pkl_root, 'kse_article_pool.pkl')
-pkl_question_pool = os.path.join(pkl_root, 'kse_question_pool.pkl')
-pkl_private_question_pool = os.path.join(pkl_root, 'kse_private_test_question_pool.pkl')
-pkl_bm25okapi = os.path.join(pkl_root, 'kse_bm25okapi_v1.pkl')
-pkl_cached_rel = os.path.join(pkl_root, 'kse_cached_rel_v2_top50.pkl')
-pkl_private_cached_rel = os.path.join(pkl_root, 'kse_private_cached_rel_v1_top50.pkl')
-pkl_split_ids = os.path.join(pkl_root, 'kse_split_ids.pkl')
-pkl_tfidf = os.path.join('pkl_file', 'kse_tfidf.pkl')
+pkl_article_pool = os.path.join(pkl_root, 'alqac_2022_article_pool.pkl')
+pkl_question_pool = os.path.join(pkl_root, 'alqac_2022_question_pool.pkl')
+pkl_private_question_pool = os.path.join(pkl_root, 'alqac_2022_private_test_question_pool.pkl')
+pkl_bm25okapi = os.path.join(pkl_root, 'alqac_2022_bm25okapi_v1.pkl')
+pkl_cached_rel = os.path.join(pkl_root, 'alqac_2022_cached_rel_v1_top100.pkl')
+pkl_private_cached_rel = os.path.join(pkl_root, 'alqac_2022_private_cached_rel_v1_top50.pkl')
+pkl_split_ids = os.path.join(pkl_root, 'alqac_2022_split_ids.pkl')
+pkl_tfidf = os.path.join(pkl_root, 'alqac_2022_tfidf.pkl')
