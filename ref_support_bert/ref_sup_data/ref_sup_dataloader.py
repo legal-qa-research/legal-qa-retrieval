41341d0f70415583436953cf9c102ec76d28f8c0
Long Nguyen <anonyhost@gmail.com>
2022-06-16 15:25:02 +0700

    update ref sup data

--- ref_support_bert/ref_sup_data/ref_sup_dataloader.py
+++ ref_support_bert/ref_sup_data/ref_sup_dataloader.py
@@ -3,16 +3,17 @@ from typing import List
 import torch
 from sentence_transformers import SentenceTransformer
 from torch import Tensor
+from torch.nn.utils.rnn import PackedSequence, pack_sequence
 from torch.utils.data import Dataset, DataLoader
 from ref_support_bert.ref_sup_data.ref_sup_sample import RefSupSample, create_lis_sample
 from ref_support_bert.args_management import args
 
 
 class RefSupDataset(Dataset):
-    def __init__(self, lis_sample: List[List[Tensor]]):
+    def __init__(self, lis_sample: List[PackedSequence]):
         self.lis_sample = lis_sample
 
-    def __getitem__(self, index) -> List[Tensor]:
+    def __getitem__(self, index) -> PackedSequence:
         return self.lis_sample[index]
 
     def __len__(self):
@@ -28,6 +29,6 @@ def get_ref_sup_dataloader():
         for sample in ref_sup_sample:
             lis_text: List[str] = [sample.query_text] + [article for article in sample.lis_article]
             lis_encode_vec: List[Tensor] = encoder.encode(lis_text, convert_to_tensor=True, convert_to_numpy=False)
-            lis_data.append(lis_encode_vec)
+            lis_data.append(pack_sequence(lis_encode_vec))
     ref_sup_dataset = RefSupDataset(lis_data)
     return DataLoader(dataset=ref_sup_dataset, batch_size=None, sampler=None)
