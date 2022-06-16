374d045e2dad06d43d27f908de4a025c434d7a81
Long Nguyen <anonyhost@gmail.com>
2022-06-16 15:55:03 +0700

    update ref sup data

--- ref_support_bert/ref_sup_data/ref_sup_dataloader.py
+++ ref_support_bert/ref_sup_data/ref_sup_dataloader.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 import torch
 from sentence_transformers import SentenceTransformer
@@ -10,10 +10,10 @@ from ref_support_bert.args_management import args
 
 
 class RefSupDataset(Dataset):
-    def __init__(self, lis_sample: List[PackedSequence]):
+    def __init__(self, lis_sample: List[Tuple[PackedSequence, float]]):
         self.lis_sample = lis_sample
 
-    def __getitem__(self, index) -> PackedSequence:
+    def __getitem__(self, index) -> Tuple[PackedSequence, float]:
         return self.lis_sample[index]
 
     def __len__(self):
@@ -24,11 +24,11 @@ def get_ref_sup_dataloader():
     ref_sup_sample = create_lis_sample()
     device: str = 'cuda' if torch.cuda.is_available() else 'cpu'
     encoder = SentenceTransformer(model_name_or_path=args.load_chk_point, device=device)
-    lis_data = []
+    lis_data: List[Tuple[PackedSequence, float]] = []
     with torch.no_grad():
         for sample in ref_sup_sample:
             lis_text: List[str] = [sample.query_text] + [article for article in sample.lis_article]
             lis_encode_vec: Tensor = encoder.encode(lis_text, convert_to_tensor=True, convert_to_numpy=False)
-            lis_data.append(pack_sequence([lis_encode_vec]))
+            lis_data.append((pack_sequence([lis_encode_vec]), 1.0))
     ref_sup_dataset = RefSupDataset(lis_data)
     return DataLoader(dataset=ref_sup_dataset, batch_size=None, sampler=None)
